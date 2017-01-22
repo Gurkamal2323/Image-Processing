e584826491de7a484821df7cb213d0c152455a0a
Gurkamal Singh <gurkamal2323@gmail.com>
2017-01-23 03:13:08 +0530

    reduce matrix size

--- 2. Reduce Matrix size/reduce_matrix_size.m
+++ 2. Reduce Matrix size/reduce_matrix_size.m
@@ -1,8 +1,12 @@
+%matrix declaration
 a = [1 2 3 4;5 6 7 8;9 10 11 12;13 14 15 16];
 [row,col] = size(a);
+%zeros(row,col) is used to create matrix with zero intial values
+%the size of matrix created by zeros() can be increased beyond its limit
 b=zeros(row/2,col/2);
 m=1;
 n=1;
+%reduce matrix size
 for i=1:2:row
     n=1;
     for j=1:2:col
