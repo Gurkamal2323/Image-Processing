1a0b8702f98d03d3ee5aef0a3cc5a0ae4c962317
Gurkamal Singh <gurkamal2323@gmail.com>
2017-01-23 03:10:23 +0530

    print matrix

--- 1. Print Matrix/print_matrix.m
+++ 1. Print Matrix/print_matrix.m
@@ -1,7 +1,12 @@
+%array declaration
+%   row 1   row 2   row 3
 a = [1 2 3;4 5 6;7 8 9];
+%size(matrix_name) returns rows and columns
 [row,col] = size(a);
+%print rows and columns to check
 row
 col
+%increment each element of matrix through loop
 for i=1:row
     for j=1:col
         a(i,j)=a(i,j)+1;
