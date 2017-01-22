b3ccf952ee4f15d371b10bf39bd7755cca64f1ca
Gurkamal Singh <gurkamal2323@gmail.com>
2017-01-23 03:15:55 +0530

    grayscale to binary

--- 4. Grayscale image to Binary (same size)/greyscal_binary.m
+++ 4. Grayscale image to Binary (same size)/greyscal_binary.m
@@ -1,5 +1,9 @@
+%method to read image
 img = imread('lena512.bmp');
+%get size of image
 [row,col] = size(img);
+%choose and threshold value and set all pixels above it to 1 (white)
+%and all pixels below it to 0 (black)
 for i=1:row
     for j=1:col
         if(img(i,j)>128)
@@ -9,4 +13,5 @@ for i=1:row
         end
     end
 end
+%writing the image to output image
 imwrite(logical(img),'lena512_bin.bmp');
